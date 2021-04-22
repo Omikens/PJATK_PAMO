919e328d96e4d987c5991c990f4a186b52422f56
Omik Futerkowski <dawidszab@wp.pl>
2021-04-22 19:17:36 +0200

    Bar chart

--- app/src/main/java/com/example/bmi/NewsScreen.java
+++ app/src/main/java/com/example/bmi/NewsScreen.java
@@ -1,5 +1,6 @@
 package com.example.bmi;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,43 +11,42 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class HomeScreen extends AppCompatActivity {
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
-    private Button buttonStart;
-    private Button buttonNews;
+import java.util.ArrayList;
+
+public class NewsScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_screen);
+        setContentView(R.layout.news_screen);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        buttonStart = (Button) findViewById(R.id.buttonStart);
-        buttonStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openMainActivity();
-            }
-        });
+        BarChart barChart = findViewById(R.id.barChart);
 
-        buttonNews = (Button) findViewById(R.id.buttonNews);
-        buttonNews.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openNewsScreen();
-            }
-        });
-    }
+        ArrayList<BarEntry> covid = new ArrayList<>();
+        covid.add(new BarEntry(2018, 0));
+        covid.add(new BarEntry(2019, 1));
+        covid.add(new BarEntry(2020, 420));
+        covid.add(new BarEntry(2021, 2137));
 
-    public void openMainActivity() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
+        BarDataSet barDataSet = new BarDataSet(covid, "Covid infections");
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barDataSet.setValueTextSize(16f);
 
-    public void openNewsScreen() {
-        Intent intent = new Intent(this, NewsScreen.class);
-        startActivity(intent);
+        BarData barData = new BarData(barDataSet);
+
+        barChart.setFitBars(true);
+        barChart.setData(barData);
+        barChart.getDescription().setText("Covid");
+        barChart.animateY(2000);
     }
 
     @Override
